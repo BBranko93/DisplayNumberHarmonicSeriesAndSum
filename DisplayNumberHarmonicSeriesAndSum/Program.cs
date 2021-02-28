4672af3bb2d6c7479391540a558f7146c0ed2ad7
BBranko93 <bane_b80@outlook.com>
2021-02-27 20:40:24 -0700

    Add project files.

---
+++ DisplayNumberHarmonicSeriesAndSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DisplayNumberHarmonicSeriesAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float i, sum = 0;
+            int n;
+            Console.WriteLine("Please enter number of terms : ");
+            n = int.Parse(Console.ReadLine());
+            
+            for(i = 1; i <= n; i++)
+            {
+                
+                sum = sum + 1 / i;
+                Console.Write("1/" + i + " + ");
+           
+            }
+            Console.WriteLine("\nSum of series up to " + n + " terms : " + sum);
+        }
+    }
+}
